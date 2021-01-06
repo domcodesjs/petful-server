c4c28f22492d3410976d642f23109a346d172546
Dominic Faulring <domcodesjs@gmail.com>
2021-01-06 14:17:51 -0800

    mod

--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -1,17 +1,15 @@
-const express = require('express')
-const json = require('body-parser').json()
+const express = require('express');
+const router = express.Router();
 
-const Pets = require('./pets.service')
-const People = require('../people/people.service')
-
-const router = express.Router()
+const Pets = require('./pets.service');
+const People = require('../people/people.service');
 
 router.get('/', (req, res) => {
   // Return all pets currently up for adoption.
-})
+});
 
 router.delete('/', json, (req, res) => {
   // Remove a pet from adoption.
-})
+});
 
-module.exports = router
+module.exports = router;
